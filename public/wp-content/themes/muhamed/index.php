2c886556c9e6a2d02cb1893d738d878e89472fe6
Goran Radosevic <gogyinsomnia@gmail.com>
2020-01-15 17:10:32 +0100

    homepate styling

--- public/wp-content/themes/muhamed/index.php
+++ public/wp-content/themes/muhamed/index.php
@@ -22,7 +22,7 @@
     <div id="container" class="container">
         <div class="row">
             <div class="col-md-3">
-                <ul>
+                <ul class="categories">
                     <li><a>THE CONQUEROR</a></li>
                     <li><a>MOUNTAIN MAP</a></li>
                     <li><a>PLANNINGS</a></li>
@@ -32,13 +32,15 @@
             </div>
             <div class="col-md-9">
                 <main id="content">
-                    <div>
+                    <div class="post-intro">
                         <h2><a href="#">Hard climbing shit.</a></h2>
-                        <p>Hard climbing shit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend [read more] 15th of april 2018 > Alan Schweitzer > MOUNTAIN-MAP</p>
+                        <img class="circle" src="/wp-content/themes/muhamed/img/post-thumbnail-1.jpg" />
+                        <p>Hard climbing shit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend [<a class="readmore">read more</a>] <span class="date">15th of april 2018 > Alan Schweitzer > MOUNTAIN-MAP</span></p>
                     </div>
-                    <div>
+                    <div class="post-intro">
                         <h2><a href="#">My paradise on earth.</a></h2>
-                        <p>My paradise on earth. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend [read more]</p>
+                        <img class="circle" src="/wp-content/themes/muhamed/img/post-thumbnail-2.jpg" />
+                        <p>My paradise on earth. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend [<a class="readmore">read more</a>]</p>
                     </div>
                 </main>
             </div>
@@ -47,7 +49,7 @@
 
     <footer id="footer" class="container-fluid">
         <div id="copyright">
-            &copy; <?php echo esc_html( date_i18n( __( 'Y', 'muhamed' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+            &copy; <?php echo esc_html( date_i18n( __( 'Y', 'muhamed' ) ) ); ?> by Adam Schweitzer&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Follow my adventures&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Imprint
         </div>
     </footer>
 
